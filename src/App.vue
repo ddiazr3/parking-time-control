9ffcd7952fba7986fb51444027a0a86d0e3d8fed
ddiazDKT <ddiazDKT@grupodkt.com>
2022-06-24 18:41:33 -0600

    sadsad

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
 <template>
   <v-fade-transition mode="out-in">
-    <v-app>
-      <v-container fluid>
-        <v-main>
-          <Header></Header>
-          <Nav></Nav>
-          <Content></Content>
-        </v-main>
-      </v-container>
-    </v-app>
+    <router-view />
   </v-fade-transition>
 </template>
 
 <script>
+  // Styles
+  import '@/styles/overrides.sass'
 
-import Header from '@/components/Layout/Header'
-import Nav from '@/components/Layout/Nav'
-import Content from '@/components/Layout/Content'
-
-export default {
-  name: 'App',
-  components: {
-    Content,
-    Nav,
-    Header
-  },
-  data: () => ({
-    //
-  })
-}
+  export default {
+    name: 'App',
+    metaInfo: {
+      title: 'App',
+      titleTemplate: '%s | Material Dashboard Free',
+      htmlAttrs: { lang: 'en' },
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      ],
+    },
+  }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   devServer: {
-    disableHostCheck: true
+    disableHostCheck: true,
   },
 
   transpileDependencies: ['vuetify'],
-  lintOnSave: false
 }
